16535222f3bbedec10a5032c1e850ca8447c7a74
llshields <laurie_shields@hotmail.com>
2015-02-21 12:42:32 -0500

    More console changes
    Added console functionality for viewing the lists
    Added functionality to load/save the files
    Added initial empty copies of the CSV files

--- Assignment 1/Assign1.vb
+++ Assignment 1/Assign1.vb
@@ -1,10 +1,24 @@
-﻿' Laurie is working on this! 
+﻿' Laurie is working on this!
+Imports Database
 Imports System.IO
 Public Module Assign1
+    ' Set up the paths for all the files - this could possibly be owned by each book as a static?
+    Const CUSTOMER_CSV_PATH = "..\..\..\customers.csv"
+    Const PRODUCTS_CSV_PATH = "..\..\..\products.csv"
+    Const ORDERS_CSV_PATH = "..\..\..\orders.csv"
+    Const CUSTOMER_XML_PATH = "..\..\..\customers.xml"
+    Const PRODUCTS_XML_PATH = "..\..\..\products.xml"
+    Const ORDERS_XML_PATH = "..\..\..\orders.xml"
+
+    ' Allocate the books
+    Dim orderbook As New OrderBook()
+    Dim productbook As New ProductBook()
+    'Dim customerbook As New CustomerBook()
 
     Sub Main()
-        LoadData()
-        MainMenu()
+        If LoadData() <> -1 Then
+            MainMenu()
+        End If
         Console.WriteLine("Press almost any key to continue")
         Console.ReadKey()
         Console.Clear()
@@ -30,14 +44,17 @@ Public Module Assign1
 
     Private Sub CustomerMenu()
         Do
-            Select Case GetChoice("Customer Menu", {"Add", "Edit", "Delete", "Return to Main Menu"})
-                Case -1, 4
+            Select Case GetChoice("Customer Menu", {"Display All Customers", "Add", "Edit", "Delete", "Return to Main Menu"})
+                Case -1, 5
                     Exit Sub
                 Case 1
-                    Console.WriteLine("Implement customer add functionality!")
+                    Console.WriteLine("Implement Display all customers functionality!")
+                    ' Console.WriteLine(customerbook.tostring())
                 Case 2
-                    Console.WriteLine("Implement customer edit functionality!")
+                    Console.WriteLine("Implement customer add functionality!")
                 Case 3
+                    Console.WriteLine("Implement customer edit functionality!")
+                Case 4
                     Console.WriteLine("Implement customer delete functionality!")
             End Select
         Loop
@@ -45,14 +62,16 @@ Public Module Assign1
 
     Private Sub ProductMenu()
         Do
-            Select Case GetChoice("Product Menu", {"Add", "Edit", "Delete", "Return to Main Menu"})
-                Case -1, 4
+            Select Case GetChoice("Product Menu", {"Display All Products", "Add", "Edit", "Delete", "Return to Main Menu"})
+                Case -1, 5
                     Exit Sub
                 Case 1
-                    Console.WriteLine("Implement product add functionality!")
+                    Console.WriteLine(productbook.tostring())
                 Case 2
-                    Console.WriteLine("Implement product edit functionality!")
+                    Console.WriteLine("Implement product add functionality!")
                 Case 3
+                    Console.WriteLine("Implement product edit functionality!")
+                Case 4
                     Console.WriteLine("Implement product delete functionality!")
             End Select
         Loop
@@ -60,16 +79,18 @@ Public Module Assign1
 
     Private Sub OrderMenu()
         Do
-            Select Case GetChoice("Order Menu", {"Add", "Edit", "Delete", "Ship", "Return to Main Menu"})
-                Case -1, 5
+            Select Case GetChoice("Order Menu", {"Display all Orders", "Add", "Edit", "Delete", "Ship", "Return to Main Menu"})
+                Case -1, 6
                     Exit Sub
                 Case 1
-                    Console.WriteLine("Implement order add functionality!")
+                    Console.WriteLine(orderbook.tostring())
                 Case 2
-                    Console.WriteLine("Implement order edit functionality!")
+                    Console.WriteLine("Implement order add functionality!")
                 Case 3
-                    Console.WriteLine("Implement order delete functionality!")
+                    Console.WriteLine("Implement order edit functionality!")
                 Case 4
+                    Console.WriteLine("Implement order delete functionality!")
+                Case 5
                     Console.WriteLine("Implement order ship functionality!")
             End Select
         Loop
@@ -100,13 +121,47 @@ Public Module Assign1
         Loop
     End Function
 
-    Private Sub LoadData()
+    Private Function LoadData() As Int16
         ' read the various books from soap and csv
-        Console.WriteLine("DeSerialize all the books and load from csv files")
-    End Sub
+        Try
+            Select Case (GetChoice("Load Menu", {"Load from CSV", "Load from XML"}))
+                Case -1
+                    Return -1
+                Case 1
+                    ' customerbook.Load(CUSTOMER_CSV_PATH)
+                    productbook.Load(PRODUCTS_CSV_PATH)
+                    orderbook.Load(ORDERS_CSV_PATH)
+                Case 2
+                    ' customerbook.Load(CUSTOMER_XML_PATH)
+                    productbook.Load(PRODUCTS_XML_PATH)
+                    orderbook.Load(ORDERS_XML_PATH)
+            End Select
+        Catch e As InvalidDataException
+            Console.WriteLine("Invalid data file format") ' LAURIE - TODO - add name of file here!!!
+            Console.ReadKey()
+            Return -1
+        Catch e As IOException
+            Console.WriteLine("Error reading file - File may not exist")
+            Return -1
+        End Try
+        Return 0
+    End Function
+
 
     Private Sub SaveData()
         ' write out the various books to csv and soap
         Console.WriteLine("Serialize all the books and save to csv files")
+        ' If Not customerbook Is Nothing Then
+        '   book.SaveCSV(CUSTOMER_CSV_PATH)
+        '   book.SaveXML(CUSTOMER_XML_PATH)
+        ' End If
+        If Not productbook Is Nothing Then
+            productbook.SaveCSV(PRODUCTS_CSV_PATH)
+            productbook.SaveXML(PRODUCTS_XML_PATH)
+        End If
+        If Not orderbook Is Nothing Then
+            orderbook.SaveCSV(ORDERS_CSV_PATH)
+            orderbook.SaveXML(ORDERS_XML_PATH)
+        End If
     End Sub
 End Module
---
+++ Database/OrderBook.vb
@@ -0,0 +1,17 @@
+﻿<Serializable()> Public Class OrderBook : Inherits Book(Of Product)
+
+    Protected Overrides ReadOnly Property fieldcount As UShort
+        Get
+            Return 7 ' LAURIE - TODO
+        End Get
+    End Property
+
+    Protected Overrides Sub Interpret(fields As String())
+        ' Dim entry As New Product(Convert.ToInt16(fields(0)), fields(1), Convert.ToDouble(fields(2)), Convert.ToInt16(fields(3)))
+        ' book.Add(entry)
+        ' RaiseEvent
+    End Sub
+
+End Class
+
+
